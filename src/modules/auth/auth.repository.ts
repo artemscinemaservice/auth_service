43c3fc811309906a41ee4752caf058c01e4ba36d
SalischevAV <devorer77@gmail.com>
2026-03-27 11:22:23 +0100

    feat: added base prisma repository

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -1,12 +1,14 @@
+import { BaseRepository } from '@artemscinemaservice/core/repository';
 import { Injectable } from '@nestjs/common';
 import { Account } from '@prisma/generated/client';
-import { AccountCreateInput } from '@prisma/generated/models';
 
 import { PrismaService } from '@/infrastructure/prisma/prisma.service';
 
 @Injectable()
-export class AuthRepository {
-	public constructor(private readonly prismaService: PrismaService) {}
+export class AuthRepository extends BaseRepository<PrismaService['account']> {
+	public constructor(private readonly prismaService: PrismaService) {
+		super(prismaService.account);
+	}
 
 	public async findAccountByPhoneNumber(
 		phoneNumber: string
@@ -20,9 +22,5 @@ export class AuthRepository {
 		return await this.prismaService.account.findUnique({
 			where: { email },
 		});
-	}
-
-	public async createAccount(data: AccountCreateInput) {
-		return await this.prismaService.account.create({ data });
 	}
 }
